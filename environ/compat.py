21ff9e1ac5e1cfb1493667b8c16b8a4ac32f7876
Serghei Iakovlev <egrep@protonmail.ch>
2021-08-30 23:58:19 +0300

    Add copyright notice

--- environ/compat.py
+++ environ/compat.py
@@ -1,8 +1,12 @@
-"""
-environ.compat
-~~~~~~~~~~~~~~~
-This module handles import compatibility issues
-"""
+# This file is part of the django-environ.
+#
+# Copyright (c) 2021, Serghei Iakovlev, <egrep@protonmail.ch>
+# Copyright (c) 2013-2021, Daniele Faraglia <daniele.faraglia@gmail.com>
+#
+# For the full copyright and license information, please view
+# the LICENSE file that was distributed with this source code.
+
+"""This module handles import compatibility issues."""
 
 import pkgutil
 
