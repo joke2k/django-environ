de73ed4f97cb61b123d1cbc23368577d9452035c
Serghei Iakovlev <egrep@protonmail.ch>
2021-08-31 00:13:14 +0300

    Refactor tests to use pytest and follow DRY

---
+++ tests/tests/fixtures.py
@@ -0,0 +1,63 @@
+# This file is part of the django-environ.
+#
+# Copyright (c) 2021, Serghei Iakovlev, <egrep@protonmail.ch>
+# Copyright (c) 2013-2021, Daniele Faraglia <daniele.faraglia@gmail.com>
+#
+# For the full copyright and license information, please view
+# the LICENSE.txt file that was distributed with this source code.
+
+from environ.compat import json
+
+
+class FakeEnv:
+    URL = 'http://www.google.com/'
+    POSTGRES = 'postgres://uf07k1:wegauwhg@ec2-107-21-253-135.compute-1.amazonaws.com:5431/d8r82722'
+    MYSQL = 'mysql://bea6eb0:69772142@us-cdbr-east.cleardb.com/heroku_97681?reconnect=true'
+    MYSQLGIS = 'mysqlgis://user:password@127.0.0.1/some_database'
+    SQLITE = 'sqlite:////full/path/to/your/database/file.sqlite'
+    ORACLE_TNS = 'oracle://user:password@sid/'
+    ORACLE = 'oracle://user:password@host:1521/sid'
+    CUSTOM_BACKEND = 'custom.backend://user:password@example.com:5430/database'
+    REDSHIFT = 'redshift://user:password@examplecluster.abc123xyz789.us-west-2.redshift.amazonaws.com:5439/dev'
+    MEMCACHE = 'memcache://127.0.0.1:11211'
+    REDIS = 'rediscache://127.0.0.1:6379/1?client_class=django_redis.client.DefaultClient&password=secret'
+    EMAIL = 'smtps://user@domain.com:password@smtp.example.com:587'
+    JSON = dict(one='bar', two=2, three=33.44)
+    DICT = dict(foo='bar', test='on')
+    PATH = '/home/dev'
+    EXPORTED = 'exported var'
+
+    @classmethod
+    def generateData(cls):
+        return dict(STR_VAR='bar',
+                    MULTILINE_STR_VAR='foo\\nbar',
+                    INT_VAR='42',
+                    FLOAT_VAR='33.3',
+                    FLOAT_COMMA_VAR='33,3',
+                    FLOAT_STRANGE_VAR1='123,420,333.3',
+                    FLOAT_STRANGE_VAR2='123.420.333,3',
+                    BOOL_TRUE_VAR='1',
+                    BOOL_TRUE_VAR2='True',
+                    BOOL_FALSE_VAR='0',
+                    BOOL_FALSE_VAR2='False',
+                    PROXIED_VAR='$STR_VAR',
+                    INT_LIST='42,33',
+                    INT_TUPLE='(42,33)',
+                    STR_LIST_WITH_SPACES=' foo,  bar',
+                    EMPTY_LIST='',
+                    DICT_VAR='foo=bar,test=on',
+                    DATABASE_URL=cls.POSTGRES,
+                    DATABASE_MYSQL_URL=cls.MYSQL,
+                    DATABASE_MYSQL_GIS_URL=cls.MYSQLGIS,
+                    DATABASE_SQLITE_URL=cls.SQLITE,
+                    DATABASE_ORACLE_URL=cls.ORACLE,
+                    DATABASE_ORACLE_TNS_URL=cls.ORACLE_TNS,
+                    DATABASE_REDSHIFT_URL=cls.REDSHIFT,
+                    DATABASE_CUSTOM_BACKEND_URL=cls.CUSTOM_BACKEND,
+                    CACHE_URL=cls.MEMCACHE,
+                    CACHE_REDIS=cls.REDIS,
+                    EMAIL_URL=cls.EMAIL,
+                    URL_VAR=cls.URL,
+                    JSON_VAR=json.dumps(cls.JSON),
+                    PATH_VAR=cls.PATH,
+                    EXPORTED_VAR=cls.EXPORTED)
---
+++ tests/tests/test_schema.py
@@ -0,0 +1,54 @@
+# This file is part of the django-environ.
+#
+# Copyright (c) 2021, Serghei Iakovlev, <egrep@protonmail.ch>
+# Copyright (c) 2013-2021, Daniele Faraglia <daniele.faraglia@gmail.com>
+#
+# For the full copyright and license information, please view
+# the LICENSE.txt file that was distributed with this source code.
+
+import os
+
+from environ import Env
+from .fixtures import FakeEnv
+
+_old_environ = None
+
+
+def setup_module():
+    """Setup environment variables to the execution for the current module."""
+    global _old_environ
+
+    _old_environ = os.environ
+    os.environ = Env.ENVIRON = FakeEnv.generateData()
+
+
+def teardown_module():
+    """Restore environment variables was previously setup in setup_module."""
+    global _old_environ
+
+    assert _old_environ is not None
+    os.environ = _old_environ
+
+
+def test_schema():
+    env = Env(INT_VAR=int, NOT_PRESENT_VAR=(float, 33.3), STR_VAR=str,
+              INT_LIST=[int], DEFAULT_LIST=([int], [2]))
+
+    assert isinstance(env('INT_VAR'), int)
+    assert env('INT_VAR') == 42
+
+    assert isinstance(env('NOT_PRESENT_VAR'), float)
+    assert env('NOT_PRESENT_VAR') == 33.3
+
+    assert 'bar' == env('STR_VAR')
+    assert 'foo' == env('NOT_PRESENT2', default='foo')
+
+    assert isinstance(env('INT_LIST'), list)
+    assert env('INT_LIST') == [42, 33]
+
+    assert isinstance(env('DEFAULT_LIST'), list)
+    assert env('DEFAULT_LIST') == [2]
+
+    # Override schema in this one case
+    assert isinstance(env('INT_VAR', cast=str), str)
+    assert env('INT_VAR', cast=str) == '42'
