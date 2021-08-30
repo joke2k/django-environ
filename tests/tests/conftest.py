a06a0a85f8eecef0b51ff214014b833e9b538720
Serghei Iakovlev <egrep@protonmail.ch>
2021-08-31 00:13:14 +0300

    Refactor tests to use pytest and follow DRY

---
+++ tests/tests/conftest.py
@@ -0,0 +1,63 @@
+# This file is part of the django-environ.
+#
+# Copyright (c) 2021, Serghei Iakovlev, <egrep@protonmail.ch>
+# Copyright (c) 2013-2021, Daniele Faraglia <daniele.faraglia@gmail.com>
+#
+# For the full copyright and license information, please view
+# the LICENSE.txt file that was distributed with this source code.
+
+import os
+import pathlib
+import sys
+
+import pytest
+
+
+@pytest.fixture
+def solr_url():
+    """Return Solr URL."""
+    return 'solr://127.0.0.1:8983/solr'
+
+
+@pytest.fixture
+def elasticsearch_url():
+    """Return Elasticsearch URL."""
+    return 'elasticsearch://127.0.0.1:9200/index'
+
+
+@pytest.fixture
+def whoosh_url():
+    """Return Whoosh URL."""
+    return 'whoosh:///home/search/whoosh_index'
+
+
+@pytest.fixture
+def xapian_url():
+    """Return Xapian URL."""
+    return 'xapian:///home/search/xapian_index'
+
+
+@pytest.fixture
+def simple_url():
+    """Return simple URL."""
+    return 'simple:///'
+
+
+@pytest.fixture
+def volume():
+    """Return volume name is OS is Windows, otherwise None."""
+    if sys.platform == 'win32':
+        return pathlib.Path(os.getcwd()).parts[0]
+    return None
+
+
+@pytest.fixture(params=[
+    'solr://127.0.0.1:8983/solr',
+    'elasticsearch://127.0.0.1:9200/index',
+    'whoosh:///home/search/whoosh_index',
+    'xapian:///home/search/xapian_index',
+    'simple:///'
+])
+def search_url(request):
+    """Return Search Engine URL."""
+    return request.param
