aada67206599ff3e4731b0480fc139a954f6bc3c
Serghei Iakovlev <egrep@protonmail.ch>
2021-08-31 12:04:47 +0300

    Add missed classifiers

--- setup.py
+++ setup.py
@@ -136,6 +136,8 @@ CLASSIFIERS = [
 
     'Programming Language :: Python',
     'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
