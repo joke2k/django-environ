3cebc499385e4d77f6fdf9bdbbacff478b6bb106
Serghei Iakovlev <egrep@protonmail.ch>
2022-06-14 14:55:42 +0200

    Correct code style

--- environ/environ.py
+++ environ/environ.py
@@ -88,8 +88,10 @@ class Env:
         )
 
         # Set the project base directory
-        BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        
+        BASE_DIR = os.path.dirname(
+            os.path.dirname(os.path.abspath(__file__))
+        )
+
         # Take environment variables from .env file
         environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
