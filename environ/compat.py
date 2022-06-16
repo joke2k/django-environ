c1b7034bbd54c548f48f8d19c882c772a29f394b
Serghei Iakovlev <egrep@protonmail.ch>
2022-06-16 09:47:50 +0200

    Provide a function to return RedisCache driver

--- environ/compat.py
+++ environ/compat.py
@@ -25,14 +25,17 @@ else:
     class ImproperlyConfigured(Exception):
         pass
 
-# use built-in support if Django 4+
-if DJANGO_VERSION is not None and DJANGO_VERSION >= (4, 0):
-    REDIS_DRIVER='django.core.cache.backends.redis.RedisCache'
-# back compatibility with redis_cache package
-elif find_loader('redis_cache'):
-    REDIS_DRIVER = 'redis_cache.RedisCache'
-else:
-    REDIS_DRIVER = 'django_redis.cache.RedisCache'
+
+def choose_rediscache_driver():
+    """Backward compatibility for RedisCache driver."""
+    # use built-in support if Django 4+
+    if DJANGO_VERSION is not None and DJANGO_VERSION >= (4, 0):
+        return 'django.core.cache.backends.redis.RedisCache'
+    # back compatibility with redis_cache package
+    elif find_loader('redis_cache'):
+        return 'redis_cache.RedisCache'
+    else:
+        return 'django_redis.cache.RedisCache'
 
 
 def choose_postgres_driver():
@@ -52,6 +55,9 @@ def choose_pymemcache_driver():
         return 'django.core.cache.backends.memcached.PyLibMCCache'
     return 'django.core.cache.backends.memcached.PyMemcacheCache'
 
+
+REDIS_DRIVER = choose_rediscache_driver()
+"""The name of the RedisCache driver."""
 
 DJANGO_POSTGRES = choose_postgres_driver()
 """The name of the PostgreSQL driver."""
