2527194a6e21e9fce8f4450685dd4aa9ccde33ac
wongcht <wongchunhong.work@gmail.com>
2023-08-27 20:35:08 +0100

    use importlib.util.find_spec

--- environ/compat.py
+++ environ/compat.py
@@ -8,14 +8,14 @@
 
 """This module handles import compatibility issues."""
 
-from pkgutil import find_loader
+from importlib.util import find_spec
 
-if find_loader('simplejson'):
+if find_spec('simplejson'):
     import simplejson as json
 else:
     import json
 
-if find_loader('django'):
+if find_spec('django'):
     from django import VERSION as DJANGO_VERSION
     from django.core.exceptions import ImproperlyConfigured
 else:
@@ -29,7 +29,7 @@ def choose_rediscache_driver():
     """Backward compatibility for RedisCache driver."""
 
     # django-redis library takes precedence
-    if find_loader('django_redis'):
+    if find_spec('django_redis'):
         return 'django_redis.cache.RedisCache'
 
     # use built-in support if Django 4+
@@ -51,7 +51,7 @@ def choose_postgres_driver():
 def choose_pymemcache_driver():
     """Backward compatibility for pymemcache."""
     old_django = DJANGO_VERSION is not None and DJANGO_VERSION < (3, 2)
-    if old_django or not find_loader('pymemcache'):
+    if old_django or not find_spec('pymemcache'):
         # The original backend choice for the 'pymemcache' scheme is
         # unfortunately 'pylibmc'.
         return 'django.core.cache.backends.memcached.PyLibMCCache'
