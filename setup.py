b98009b81063ba4e831ecd56afd15e6fc961a529
Serghei Iakovlev <git@serghei.pl>
2024-10-25 13:08:01 +0200

    Drop Django 1.x support

--- setup.py
+++ setup.py
@@ -135,9 +135,6 @@ CLASSIFIERS = [
     'Development Status :: 5 - Production/Stable',
 
     'Framework :: Django',
-    'Framework :: Django :: 1.11',
-    'Framework :: Django :: 2.0',
-    'Framework :: Django :: 2.1',
     'Framework :: Django :: 2.2',
     'Framework :: Django :: 3.0',
     'Framework :: Django :: 3.1',
