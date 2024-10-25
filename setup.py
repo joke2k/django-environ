b1ab7729783aa2271b17eb78d10fb488dda79a9a
Serghei Iakovlev <git@serghei.pl>
2024-10-25 11:54:31 +0200

    Add support for Python 3.13 and drop support for 3.6-3.8

--- setup.py
+++ setup.py
@@ -154,13 +154,11 @@ CLASSIFIERS = [
 
     'Programming Language :: Python',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
     'Programming Language :: Python :: 3.10',
     'Programming Language :: Python :: 3.11',
     'Programming Language :: Python :: 3.12',
+    'Programming Language :: Python :: 3.13',
     'Programming Language :: Python :: Implementation :: CPython',
     'Programming Language :: Python :: Implementation :: PyPy',
 
